2607d98509ae453c1dd3fd0584e86c6f0e5ef5ee
eternal-kalamity <manche881@gmail.com>
2025-09-20 12:55:21 -0700

    1.5 opened book with main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(bookpath):
+    with open(bookpath, encoding='utf-8') as f:
+        return f.read()
+
+def main():
+    bookpath = "books/frankenstein.txt"
+
+    booktext = get_book_text(bookpath)
+
+    print(booktext)
+
+main()
