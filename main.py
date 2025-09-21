0a9d72a2bf6a63f84d571acf157c1dd5fa96a816
eternal-kalamity <manche881@gmail.com>
2025-09-21 09:16:01 -0700

    1.8 added char counter to stats.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #import os
-from stats import word_count
+from stats import word_count, char_counter
 
 def get_book_text(bookpath):
     with open(bookpath, encoding='utf-8') as f:
@@ -12,6 +12,11 @@ def main():
 
     num_words = word_count(booktext)
 
+    char_count = char_counter(booktext)
+
     print(f"{num_words} words found in the document")
+
+#    for char in char_count:
+    print(f"{char_count}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
-def word_count(words):
+def word_count(words: str) -> int:
 #   with open(bookpath) as f: -- this is also fine --
     return len(words.split())
+
+def char_counter(words: str) -> int:
+#   
+    char_count = {}
+    for char in words:
+        char_count[char] = char_count.get(char, 0) + 1
+    return char_count
